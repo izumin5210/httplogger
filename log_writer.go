a194c61f8e2a4592ea35af408e151346ced34e88
izumin5210 <masayuki@izumin.info>
2017-09-12 23:23:48 +0900

    Define RequestLog and ResponseLog

--- log_writer.go
+++ log_writer.go
@@ -1,5 +1,23 @@
 package httplogger
 
+import (
+	"net/http"
+	"time"
+)
+
+// RequestLog contains http(s) request information
+type RequestLog struct {
+	*http.Request
+	RequestedAt time.Time
+}
+
+// RequestLog contains http(s) response information or errors
+type ResponseLog struct {
+	*http.Response
+	DurationNano int64
+	Error        error
+}
+
 // LogWriter is interface for writing logs
 type LogWriter interface {
 	Println(v ...interface{})
